7971f0eb914c5f65eff84caaf59a36ad982e9899
heeso <khs@alpinelab.io>
2024-07-23 11:53:49 +0900

    📦 라우터 수정

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -7,9 +7,9 @@ import Home from "@/pages/Home";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
+      <Route index element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
+      <Route path="/home" element={<Home />} />
     </Route>
   )
 );
