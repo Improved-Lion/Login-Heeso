b959793105b58161c2d6cc1b70d51f417ccfbf9d
heeso <khs@alpinelab.io>
2024-08-01 11:07:32 +0900

    ✨ 중복 이메일, 닉네임 검사, 에러메세지 🌊 형식 검사 상태 하나로 통합

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,9 +1,9 @@
-import { Flex, Text, FormControl, FormLabel, Input, Button, VStack, useToast, FormErrorMessage } from "@chakra-ui/react";
+import { Flex, Text, FormControl, FormLabel, Input, Button, VStack, useToast, FormErrorMessage, useAnimationState } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import PocketBase from 'pocketbase';
 import { useNavigate } from 'react-router-dom';
 import { Link as ChakraLink } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
+import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
@@ -14,11 +14,18 @@ function SignUp() {
   const [ confirmPassword, setConfirmPassword ] = useState<string>('');
   const [ nickname, setNickname ] = useState<string>('');
 
-  // 형식 검사 상태
-  const [ emailError, setEmailError ] = useState<boolean>(false);
-  const [ passwordError, setPasswordError ] = useState<boolean>(false);
-  const [ passwordConfirmError, setPasswordConfirmError ] = useState<boolean>(false);
-  const [ nicknameError, setNicknameError ] = useState<boolean>(false);
+  // // 형식 검사 상태
+  // const [ emailError, setEmailError ] = useState<boolean>(false);
+  // const [ passwordError, setPasswordError ] = useState<boolean>(false);
+  // const [ passwordConfirmError, setPasswordConfirmError ] = useState<boolean>(false);
+  // const [ nicknameError, setNicknameError ] = useState<boolean>(false);
+
+  const [ errors, setErrors ] = useState({
+    email: false,
+    password: false,
+    confirmPassword: false,
+    nickname: false,
+  })
 
   // 회원가입 실패 시 에러 메세지 상태
   const [ errorMessage, setErrorMessage ] = useState<string>('');
@@ -30,54 +37,64 @@ function SignUp() {
   // 위의 유효성 검사 통과 시에만 제출 버튼 활성화 ( 아직 )
 
   // 형식 검사
-  const emailValidation = (email: string) => {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-    return emailRegex.test(email);
-  };
-
-  const passwordValidation = (password :string) => {
-    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,15}$/;
-    return passwordRegex.test(password);
-  };
-
-  const passwordConfirmCheck = (confirmPassword :string) => {
-    return password === confirmPassword ? true : false
-  };
-
-  const nicknameValidation = (nickname: string) => {
-    const nicknameRegex = /^.{2,6}$/;
-    return nicknameRegex.test(nickname);
-  };
+  const emailValidation = (email: string) => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(email);
+  const passwordValidation = (password :string) => /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,15}$/.test(password);
+  const passwordConfirmCheck = (confirmPassword :string) => password === confirmPassword;
+  const nicknameValidation = (nickname: string) => /^.{2,6}$/.test(nickname);
 
   useEffect(() => {
-    email && !emailValidation(email) ?
-    setEmailError(true) : setEmailError(false);
-    
-    password && !passwordValidation(password) ?
-    setPasswordError(true) : setPasswordError(false);
-
-    confirmPassword && !passwordConfirmCheck(confirmPassword) ?
-    setPasswordConfirmError(true) : setPasswordConfirmError(false);
-
-    nickname && !nicknameValidation(nickname) ?
-    setNicknameError(true) : setNicknameError(false);
-
-  }, [email, password, confirmPassword, nickname])
+    setErrors({
+        email: email ? !emailValidation(email) : false,
+        password : password ? !passwordValidation(password) : false,
+        confirmPassword: confirmPassword ? !passwordConfirmCheck(confirmPassword) : false,
+        nickname: nickname ? !nicknameValidation(nickname) : false,
+      });
+    }, [email, password, confirmPassword, nickname]);
 
   // 클릭 시 해야할 일
   // 이메일 중복체크 : 에러메세지 출력
   // 닉네임 중복체크 : 에러메세지 출력
 
-  // const checkEmailExists = async (email: string) => {
-  //   const result = await pb.collection('users').getFullList()
-  // }
+  const emailCheck = async (email : string) => {
+    try {
+      const records = await pb.collection('users').getFullList();
+      const emailList = records.map( user => user.email );
+      return emailList.includes(email);
+    } catch (error) {
+      console.error('email error', error);
+      return false;
+    }
+  };
+
+  const nicknameCheck = async (nickname : string) => {
+    try {
+      const records = await pb.collection('users').getFullList();
+      const nicknameList = records.map( user => user.name );
+      return nicknameList.includes(nickname);
+    } catch (error) {
+      console.error('nickname error', error);
+      return false;
+    }
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
 
     e.preventDefault();
 
-    if (emailError || passwordError || passwordConfirmError || nicknameError) {
+    if (Object.values(errors).some(error => error)) {
+      setErrorMessage('Please check you passed all validation.');
       return;
+    }
+
+    const isEmailTaken = await emailCheck(email);
+    const isNickNameTaken = await nicknameCheck(nickname);
+
+    if( isEmailTaken && isNickNameTaken ) {
+      setErrorMessage('This email is already in use and this nickname is already taken. Please try another.');
+    } else if ( isEmailTaken ) {
+      setErrorMessage('This email is already in use. Please try another.');
+    } else if ( isNickNameTaken ) {
+      setErrorMessage('This nickname is already taken. Please choose another.');
     }
 
     try {
@@ -100,7 +117,7 @@ function SignUp() {
       console.error(error);
       toast({
         title: 'Register Failed',
-        description: "Make sure you passed all validation and try again!",
+        description: errorMessage,
         duration: 5000,
         isClosable: true,
         status: 'error'
@@ -118,53 +135,53 @@ function SignUp() {
 
       <VStack spacing={6} w='100%'>
         {/* isInvalid : 에러 상태인지 여부 결정,  */}
-        <FormControl isInvalid={emailError} isRequired>
+        <FormControl isInvalid={errors.email} isRequired>
           <FormLabel>Email</FormLabel>
           <Input
             id="email" type="email"
             placeholder="example12@gmail.com"
             onChange={(e) => setEmail(e.target.value)}
           />
-          {emailError &&
+          {errors.email &&
           <FormErrorMessage>
             Please enter a valid email address.
           </FormErrorMessage>
           }
         </FormControl>
 
-        <FormControl isInvalid={passwordError} isRequired>
+        <FormControl isInvalid={errors.password} isRequired>
           <FormLabel>Password</FormLabel>
           <Input
             id="password" type="password" placeholder="password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          {passwordError &&
+          {errors.password &&
           <FormErrorMessage>
             Password must be between 8 and 15 characters long and include both letters and numbers.
           </FormErrorMessage>
           }
         </FormControl>
 
-        <FormControl isInvalid={passwordConfirmError} isRequired>
+        <FormControl isInvalid={errors.confirmPassword} isRequired>
           <FormLabel>Confirm Password</FormLabel>
           <Input
             id="confirmPassword" type="password" placeholder="password"
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
-          {passwordConfirmError &&
+          {errors.confirmPassword &&
           <FormErrorMessage>
             Passwords do not match. Please try again.
           </FormErrorMessage>
           }
         </FormControl>
 
-        <FormControl isInvalid={nicknameError} isRequired>
+        <FormControl isInvalid={errors.nickname} isRequired>
           <FormLabel>Nickname</FormLabel>
           <Input
             id="nickname" type="name" placeholder="nickname"
             onChange={(e) => setNickname(e.target.value)}
           />
-          {nicknameError &&
+          {errors.nickname &&
           <FormErrorMessage>
             Nickname must be between 2 and 6 characters long.
           </FormErrorMessage>
