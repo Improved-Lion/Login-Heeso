8609cc13dc87dd7240a5a2376ade83cf1c5f0bdf
heeso <khs@alpinelab.io>
2024-08-13 17:48:54 +0900

    🌊

--- frontend/src/layout/RootLayout.tsx
+++ frontend/src/layout/RootLayout.tsx
@@ -1,13 +1,19 @@
 import { FC, ReactElement } from 'react';
 import { Outlet } from "react-router-dom";
 import { Flex } from '@chakra-ui/react';
+import Header from '@/components/Header';
 
 const RootLayout: FC = (): ReactElement => {
+
   return (
-    <Flex justifyContent={'center'} p={20}>
-      <main>
-        <Outlet />
-      </main>
+    <Flex
+      direction={'column'}
+      justifyContent={'center'} alignItems={'center'}
+      p={20}
+      gap={5}
+    >
+      <Header />
+      <Outlet />
     </Flex>
   );
 };
