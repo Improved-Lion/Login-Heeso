72db6d005414fdd2e1a7a37ae1382246831da93f
heeso <khs@alpinelab.io>
2024-08-13 17:49:28 +0900

    🔥

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,34 +1,21 @@
-import { Flex, Text, Button } from "@chakra-ui/react";
-import { useNavigate } from 'react-router-dom';
-import PocketBase from 'pocketbase';
-import useUserStore from "@/store/useUserStore";
-
-const pb = new PocketBase('http://127.0.0.1:8090');
+import { Flex } from '@chakra-ui/react';
+import PostList from '@/components/PostList';
+import { Button } from '@chakra-ui/react';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 function Home() {
-  const { user, logout } = useUserStore();
-  const navigate = useNavigate();
-  console.log(user);
 
-  const handleLogout = async () => {
-    try {
-      await pb.authStore.clear();
-      logout();
-      navigate('/');
-    } catch (error) {
-      console.error("Logout failed : ",error)
-    }
-    
-  }
 
   return(
-    <Flex direction={"column"} justifyContent={'center'} alignItems={"center"} maxW='500px' gap={4}>
-      <Text fontSize={"x-large"}>Welcome !</Text>
-      <Text>User Information</Text>
-      <Text>Your email : {user?.email ?? 'No'}</Text>
-      <Text>Your name: {user?.name ?? 'No'}</Text>
-      <Text w={500}>Your token: {user?.token ?? 'No'}</Text>
-      <Button onClick={handleLogout}>Log out</Button>
+    <Flex direction={'column'} gap={5}>
+      <PostList />
+      <ChakraLink as={ReactRouterLink} to='/create'>
+        <Button w='100%'>
+          글 쓰기
+        </Button>
+      </ChakraLink>
+      
     </Flex>
   )
 }
